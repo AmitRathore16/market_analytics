f1d8fc08ee22d1adc37bdbc00dd5ada19ac0c5f2
AmitRathore16 <2023kuec2041@iiitkota.ac.in>
2026-06-06 18:37:26 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,4 @@ app.post('/api/refresh-data', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
